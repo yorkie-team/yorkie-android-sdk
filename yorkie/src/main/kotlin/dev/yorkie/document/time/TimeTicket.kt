fadb95d7da7518c5af77eb40d8ca515a0b9c8cae
Hyowoo Kim <hyowoo.kim@navercorp.com>
2022-10-06 08:55:13 +0900

    enable comparisons of nullable TimeTickets.

--- yorkie/src/main/kotlin/dev/yorkie/document/time/TimeTicket.kt
+++ yorkie/src/main/kotlin/dev/yorkie/document/time/TimeTicket.kt
@@ -1,5 +1,8 @@
 package dev.yorkie.document.time
 
+import dev.yorkie.document.time.ActorID.Companion.INITIAL_ACTOR_ID
+import dev.yorkie.document.time.ActorID.Companion.MAX_ACTOR_ID
+
 /**
  * [TimeTicket] is a timestamp of the logical clock. Ticket is immutable.
  */
@@ -9,38 +12,10 @@ internal data class TimeTicket(
     val actorID: ActorID,
 ) : Comparable<TimeTicket> {
 
-    fun toIDString(): String {
-        return "$lamport:$actorID:$delimiter"
-    }
-
-    override fun toString(): String {
-        return toIDString()
-    }
-
     override fun compareTo(other: TimeTicket): Int {
         return lamport.compareTo(other.lamport).takeUnless { it == 0 }
             ?: actorID.compareTo(other.actorID).takeUnless { it == 0 }
             ?: delimiter.compareTo(delimiter)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as TimeTicket
-
-        if (lamport != other.lamport) return false
-        if (delimiter != other.delimiter) return false
-        if (actorID != other.actorID) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = lamport.hashCode()
-        result = 31 * result + delimiter
-        result = 31 * result + actorID.hashCode()
-        return result
     }
 
     companion object {
@@ -48,7 +23,15 @@ internal data class TimeTicket(
         const val MAX_DELIMITER = Int.MAX_VALUE
         const val MAX_LAMPORT = Long.MAX_VALUE
 
-        val InitialTimeTicket = TimeTicket(0, INITIAL_DELIMITER, ActorID.INITIAL_ACTOR_ID)
-        val MaxTimeTicket = TimeTicket(MAX_LAMPORT, MAX_DELIMITER, ActorID.MAX_ACTOR_ID)
+        private val NullTimeTicket = TimeTicket(Long.MIN_VALUE, INITIAL_DELIMITER, INITIAL_ACTOR_ID)
+
+        val InitialTimeTicket = TimeTicket(0, INITIAL_DELIMITER, INITIAL_ACTOR_ID)
+        val MaxTimeTicket = TimeTicket(MAX_LAMPORT, MAX_DELIMITER, MAX_ACTOR_ID)
+
+        operator fun TimeTicket?.compareTo(other: TimeTicket?): Int {
+            return orNull().compareTo(other.orNull())
+        }
+
+        private fun TimeTicket?.orNull() = this ?: NullTimeTicket
     }
 }

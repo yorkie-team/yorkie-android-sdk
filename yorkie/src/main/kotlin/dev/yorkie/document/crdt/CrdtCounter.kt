b2a4252de67feb61e4b80864963b201c64cb967b
Hyowoo Kim <skhugh@gmail.com>
2022-10-06 10:06:36 +0900

    Implement CrdtCounter (#14)

---
+++ yorkie/src/main/kotlin/dev/yorkie/document/crdt/CrdtCounter.kt
@@ -0,0 +1,81 @@
+package dev.yorkie.document.crdt
+
+import dev.yorkie.document.time.TimeTicket
+import java.nio.ByteBuffer
+
+typealias CounterValue = Number
+
+/**
+ * [CrdtCounter] represents changeable number data type.
+ */
+internal class CrdtCounter private constructor(
+    value: CounterValue,
+    createdAt: TimeTicket,
+) : CrdtElement(createdAt) {
+    var value = value.sanitized()
+        private set(value) {
+            field = value.sanitized()
+        }
+
+    val type
+        get() = value.counterType()
+
+    fun toBytes(): ByteArray {
+        return when (type) {
+            CounterType.IntegerCnt -> ByteBuffer.allocate(Int.SIZE_BYTES).putInt(value.toInt())
+            CounterType.LongCnt -> ByteBuffer.allocate(Long.SIZE_BYTES).putLong(value.toLong())
+            CounterType.DoubleCnt -> {
+                ByteBuffer.allocate(Double.SIZE_BYTES).putDouble(value.toDouble())
+            }
+        }.array()
+    }
+
+    fun increase(primitive: Primitive) {
+        val primitiveValue = primitive.value
+        require(primitive.isNumericType && primitiveValue is Number) {
+            "Unsupported type of value: ${primitive.type}"
+        }
+        val increaseResult = value.toDouble() + primitiveValue.toDouble()
+        value = when (type) {
+            CounterType.IntegerCnt -> {
+                if (increaseResult < Int.MIN_VALUE || increaseResult > Int.MAX_VALUE) {
+                    increaseResult.toLong()
+                } else {
+                    increaseResult.toInt()
+                }
+            }
+            CounterType.LongCnt -> increaseResult.toLong()
+            CounterType.DoubleCnt -> increaseResult
+        }
+    }
+
+    companion object {
+
+        fun of(value: CounterValue, createdAt: TimeTicket) = CrdtCounter(value, createdAt)
+
+        fun CounterValue.sanitized(): Number = when (counterType()) {
+            CounterType.IntegerCnt -> toInt()
+            CounterType.LongCnt -> toLong()
+            CounterType.DoubleCnt -> toDouble()
+        }
+
+        fun CounterValue.counterType() = when (this) {
+            is Byte, is Short, is Int -> CounterType.IntegerCnt
+            is Long -> CounterType.LongCnt
+            else -> CounterType.DoubleCnt
+        }
+
+        fun ByteArray.asCounterValue(counterType: CounterType): Number =
+            with(ByteBuffer.wrap(this)) {
+                when (counterType) {
+                    CounterType.IntegerCnt -> int
+                    CounterType.LongCnt -> long
+                    CounterType.DoubleCnt -> double
+                }
+            }
+    }
+
+    enum class CounterType {
+        IntegerCnt, LongCnt, DoubleCnt
+    }
+}
--- yorkie/src/main/kotlin/dev/yorkie/document/crdt/Primitive.kt
+++ yorkie/src/main/kotlin/dev/yorkie/document/crdt/Primitive.kt
@@ -5,24 +5,28 @@ import java.util.Date
 
 internal class Primitive(
     val value: Any?,
-    val createdAtTime: TimeTicket,
+    createdAtTime: TimeTicket,
 ) : CrdtElement(createdAtTime) {
-    var type: PrimitiveType? = null
-
-    init {
-        type = when (value) {
-            is Boolean -> PrimitiveType.Boolean
-            is Int -> PrimitiveType.Integer
-            is Long -> PrimitiveType.Long
-            is Double -> PrimitiveType.Double
-            is String -> PrimitiveType.String
-            is ByteArray -> PrimitiveType.Bytes
-            is Date -> PrimitiveType.Date
-            else -> PrimitiveType.Null
-        }
+    val type = when (value) {
+        is Boolean -> PrimitiveType.Boolean
+        is Int -> PrimitiveType.Integer
+        is Long -> PrimitiveType.Long
+        is Double -> PrimitiveType.Double
+        is String -> PrimitiveType.String
+        is ByteArray -> PrimitiveType.Bytes
+        is Date -> PrimitiveType.Date
+        else -> PrimitiveType.Null
     }
 
+    val isNumericType = type in NUMERIC_TYPES
+
     companion object {
+        private val NUMERIC_TYPES = setOf(
+            PrimitiveType.Integer,
+            PrimitiveType.Long,
+            PrimitiveType.Double,
+        )
+
         /**
          * Creates a new instance of Primitive.
          */
@@ -35,6 +39,6 @@ internal class Primitive(
 /**
  * Primitive is a CRDT element that represents a primitive value.
  */
-enum class PrimitiveType {
+internal enum class PrimitiveType {
     Null, Boolean, Integer, Long, Double, String, Bytes, Date
 }

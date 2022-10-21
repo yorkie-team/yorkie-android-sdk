cc9f07ce5313c22fe242101c74a5c962b5301e44
Hyowoo Kim <hyowoo.kim@navercorp.com>
2022-10-21 16:03:33 +0900

    implement Document

---
+++ yorkie/src/main/kotlin/dev/yorkie/api/SnapshotConverter.kt
@@ -0,0 +1,12 @@
+package dev.yorkie.api
+
+import com.google.protobuf.ByteString
+import dev.yorkie.document.crdt.CrdtObject
+
+internal fun ByteString.toCrdtObject(): CrdtObject {
+    TODO("not yet implemented")
+}
+
+internal fun CrdtObject.toByteString(): ByteString {
+    TODO("not yet implemented")
+}

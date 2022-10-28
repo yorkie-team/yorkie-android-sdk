08e7c97cddbfe1e0a0dba8d8306237eb66540d4f
Hyowoo Kim <hyowoo.kim@navercorp.com>
2022-10-28 16:36:28 +0900

    add test codes for Document (#36)

---
+++ yorkie/src/test/kotlin/dev/yorkie/JsonTestUtils.kt
@@ -0,0 +1,15 @@
+// ktlint-disable filename
+
+package dev.yorkie
+
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import org.junit.Assert.assertEquals
+
+private val gson = Gson()
+
+fun assertJsonContentEquals(expected: String, actual: String) {
+    val expectedJson = gson.fromJson(expected, JsonObject::class.java)
+    val actualJson = gson.fromJson(actual, JsonObject::class.java)
+    assertEquals(expectedJson, actualJson)
+}

0ba49b366242c14d2e845312040992528d3ba1b2
Hyowoo Kim <hyowoo.kim@navercorp.com>
2022-11-08 16:14:58 +0900

    Add instrumented test (#44)
    * add instrumented test
    
    * use api 28 instead

--- yorkie/build.gradle.kts
+++ yorkie/build.gradle.kts
@@ -114,4 +114,5 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation(kotlin("test"))
 }

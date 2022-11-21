ea20bfcc32f5b18be97c0130b36be224651aef96
jee-hyun-kim <jee_hyun.kim@navercorp.com>
2022-11-21 15:57:43 +0900

    Rename FindPrefixes to Trie

---
+++ yorkie/src/main/kotlin/dev/yorkie/util/Trie.kt
@@ -0,0 +1,18 @@
+// ktlint-disable filename
+
+package dev.yorkie.util
+
+import org.apache.commons.collections4.trie.PatriciaTrie
+
+fun PatriciaTrie<String>.findPrefixes(): List<CharArray> {
+    val paths = keys.toList()
+    var prefix = ""
+    return buildList {
+        paths.forEachIndexed { index, path ->
+            if (index == 0 || !path.startsWith(prefix)) {
+                add(path.toCharArray())
+                prefix = path
+            }
+        }
+    }
+}

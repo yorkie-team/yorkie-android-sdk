357f03ca1e11fe1c6671ebf84819609923e58899
jee-hyun-kim <jee_hyun.kim@navercorp.com>
2022-12-15 18:37:50 +0900

    Fix host name

--- examples/src/main/kotlin/dev/yorkie/examples/KanbanBoardViewModel.kt
+++ examples/src/main/kotlin/dev/yorkie/examples/KanbanBoardViewModel.kt
@@ -24,7 +24,7 @@ class KanbanBoardViewModel : ViewModel() {
     val list: StateFlow<ImmutableList<KanbanColumn>> = _list.asStateFlow()
 
     fun init(context: Context) {
-        val client = Client(context, "api.yorkie.dev ", 443, true)
+        val client = Client(context, "api.yorkie.dev", 443, true)
         viewModelScope.launch {
             if (client.activateAsync().await()) {
                 client.attachAsync(document).await()

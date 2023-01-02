08010962460f1789d732013e03977f2917cabe40
Jeehyun Kim <jee_hyun.kim@navercorp.com>
2023-01-02 15:14:09 +0900

    Implement RgaTreeSplit (#59)

---
+++ yorkie/src/test/kotlin/dev/yorkie/document/crdt/RgaTreeSplitTest.kt
@@ -0,0 +1,35 @@
+package dev.yorkie.document.crdt
+
+import dev.yorkie.document.time.TimeTicket
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class RgaTreeSplitTest {
+    private lateinit var target: RgaTreeSplit<String>
+
+    @Before
+    fun setUp() {
+        target = RgaTreeSplit()
+    }
+
+    @Test
+    fun `should handle edit operations with case1`() {
+        var range = RgaTreeSplitNodeRange(target.findNodePos(0), target.findNodePos(0))
+        target.edit(range, TimeTicket.InitialTimeTicket, "ABCD")
+        range = RgaTreeSplitNodeRange(target.findNodePos(1), target.findNodePos(3))
+        target.edit(range, TimeTicket.InitialTimeTicket, "12")
+
+        assertEquals("A12D", target.toJson())
+    }
+
+    @Test
+    fun `should handle edit operations with case2`() {
+        var range = RgaTreeSplitNodeRange(target.findNodePos(0), target.findNodePos(0))
+        target.edit(range, TimeTicket.InitialTimeTicket, "ABCD")
+        range = RgaTreeSplitNodeRange(target.findNodePos(3), target.findNodePos(3))
+        target.edit(range, TimeTicket.InitialTimeTicket, "\n")
+
+        assertEquals("ABC\nD", target.toJson())
+    }
+}

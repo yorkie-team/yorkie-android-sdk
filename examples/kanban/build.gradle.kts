c194956f7c0b3b76c84dddcbb40d9445e6f45ca3
Hyowoo Kim <hyowoo.kim@navercorp.com>
2023-01-18 21:02:21 +0900

    Bump up libraries (#73)
    * bump up libraries
    
    * fix lint error
    
    * update AGP

--- examples/kanban/build.gradle.kts
+++ examples/kanban/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.4.0"
     }
     packagingOptions {
         resources {
@@ -52,20 +52,24 @@ android {
 dependencies {
     implementation(project(":yorkie"))
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui:${Versions.composeUi}")
-    implementation("androidx.compose.ui:ui-tooling-preview:${Versions.composeUi}")
-    implementation("androidx.compose.material:material:1.3.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.viewModel}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
     implementation("androidx.test:monitor:1.6.1")
-    implementation("androidx.test.ext:junit-ktx:1.1.4")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("junit:junit:4.13.2")
-    debugImplementation("androidx.compose.ui:ui-tooling:${Versions.composeUi}")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${Versions.composeUi}")
 }

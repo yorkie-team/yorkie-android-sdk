a80e4cfce3e7b8983ed2bb0337b88cfd82f7cc69
Hyowoo Kim <hyowoo.kim@navercorp.com>
2023-02-16 22:14:48 +0900

    few improvements to handle editable text

--- examples/texteditor/build.gradle.kts
+++ examples/texteditor/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
 }
 
 android {
@@ -38,7 +37,6 @@ android {
     }
 
     buildFeatures {
-        dataBinding = true
         viewBinding = true
     }
 }

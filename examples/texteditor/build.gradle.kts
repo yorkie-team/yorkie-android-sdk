7675b2a81e39907635fdc2d4b5af33377e410e8b
Hyowoo Kim <hyowoo.kim@navercorp.com>
2023-03-10 00:08:41 +0900

    bump up libraries (#89)

--- examples/texteditor/build.gradle.kts
+++ examples/texteditor/build.gradle.kts
@@ -44,11 +44,11 @@ android {
 dependencies {
     implementation(project(":yorkie"))
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.activity:activity-ktx:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

9e66cfa8d1b0c789fd54a42c084cbd0e0a73d9d6
jee-hyun-kim <jee_hyun.kim@navercorp.com>
2023-06-22 18:40:54 +0900

    add TextOpInfo

--- yorkie/src/main/kotlin/dev/yorkie/document/operation/OperationInfo.kt
+++ yorkie/src/main/kotlin/dev/yorkie/document/operation/OperationInfo.kt
@@ -13,6 +13,8 @@ public sealed class OperationInfo {
 
     internal var executedAt: TimeTicket = TimeTicket.InitialTimeTicket
 
+    public interface TextOpInfo
+
     public data class AddOpInfo(val index: Int, override var path: String = INITIAL_PATH) :
         OperationInfo()
 
@@ -39,20 +41,20 @@ public sealed class OperationInfo {
         val to: Int,
         val value: TextWithAttributes,
         override var path: String = INITIAL_PATH,
-    ) : OperationInfo()
+    ) : OperationInfo(), TextOpInfo
 
     public data class StyleOpInfo(
         val from: Int,
         val to: Int,
         val attributes: Map<String, String>,
         override var path: String = INITIAL_PATH,
-    ) : OperationInfo()
+    ) : OperationInfo(), TextOpInfo
 
     public data class SelectOpInfo(
         val from: Int,
         val to: Int,
         override var path: String = INITIAL_PATH,
-    ) : OperationInfo()
+    ) : OperationInfo(), TextOpInfo
 
     companion object {
         private const val INITIAL_PATH = "initial path"

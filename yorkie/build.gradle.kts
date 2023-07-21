ddb5aa30297e0948a57b7fe07230c87ef1c82e99
Hyowoo Kim <hyowoo.kim@navercorp.com>
2023-07-21 13:10:25 +0900

    Improve CI (#125)

--- yorkie/build.gradle.kts
+++ yorkie/build.gradle.kts
@@ -23,7 +23,15 @@ tasks.register<JacocoReport>("jacocoDebugTestReport") {
 
     classDirectories.setFrom(
         fileTree("${buildDir}/tmp/kotlin-classes/debug") {
-            exclude("**/dev/yorkie/api/v1/**")
+            exclude(
+                "**/dev/yorkie/api/v1/**",
+                "**/R.class",
+                "**/R$*.class",
+                "**/BuildConfig.*",
+                "**/Manifest*.*",
+                "**/*Test*.*",
+                "android/**/*.*",
+            )
         },
     )
     sourceDirectories.setFrom("${project.projectDir}/src/main/kotlin")
@@ -32,6 +40,7 @@ tasks.register<JacocoReport>("jacocoDebugTestReport") {
             include(
                 "jacoco/testDebugUnitTest.exec",
                 "outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec",
+                "outputs/code_coverage/debugAndroidTest/connected/**/coverage.ec",
             )
         },
     )

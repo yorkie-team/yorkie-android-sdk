8392c7143ce7ec0ec881596ee138eb57b0c272ae
김효우[SE][SmartEditor] <hyowoo.kim@navercorp.com>
2024-05-13 12:35:43 +0900

    simplify TreeNode interface

--- yorkie/src/main/kotlin/dev/yorkie/document/crdt/TreeInfo.kt
+++ yorkie/src/main/kotlin/dev/yorkie/document/crdt/TreeInfo.kt
@@ -10,63 +10,48 @@ import dev.yorkie.util.IndexTreeNode.Companion.DEFAULT_TEXT_TYPE
  */
 internal sealed class TreeNode {
     abstract val type: String
-    abstract val childNodes: List<TreeNode>?
-    abstract val value: String?
-    abstract val attributes: Map<String, String>?
 
     fun toJsonTreeNode(): JsonTree.TreeNode {
         return this as JsonTree.TreeNode
-    }
-
-    override fun toString(): String {
-        return if (type == DEFAULT_TEXT_TYPE) {
-            """{"type":"$type","value":"$value"}"""
-        } else {
-            val ssb = StringBuilder(
-                """{"type":"$type","children":[${
-                    childNodes.orEmpty().joinToString(",")
-                }]""",
-            )
-            val attributes = attributes
-            if (attributes?.isNotEmpty() == true) {
-                ssb.append(",")
-                ssb.append(
-                    """"attributes":{${
-                        attributes.entries.joinToString(",") { (key, value) ->
-                            """"$key":"$value""""
-                        }
-                    }}""",
-                )
-            }
-            ssb.append("}")
-            ssb.toString()
-        }
     }
 }
 
 internal data class TreeElementNode(
     override val type: String,
-    override val childNodes: List<TreeNode> = emptyList(),
+    val childNodes: List<TreeNode> = emptyList(),
     override val attributes: Map<String, String> = emptyMap(),
 ) : TreeNode(), JsonTree.ElementNode {
-    override val value: String? = null
 
     @Suppress("UNCHECKED_CAST")
     override val children: List<JsonTree.TreeNode> = childNodes as List<JsonTree.TreeNode>
 
     override fun toString(): String {
-        return super.toString()
+        val ssb = StringBuilder(
+            """{"type":"$type","children":[${
+                childNodes.joinToString(",")
+            }]""",
+        )
+        val attributes = attributes
+        if (attributes.isNotEmpty()) {
+            ssb.append(",")
+            ssb.append(
+                """"attributes":{${
+                    attributes.entries.joinToString(",") { (key, value) ->
+                        """"$key":"$value""""
+                    }
+                }}""",
+            )
+        }
+        ssb.append("}")
+        return ssb.toString()
     }
 }
 
 internal data class TreeTextNode(override val value: String = "") : TreeNode(), JsonTree.TextNode {
     override val type: String = DEFAULT_TEXT_TYPE
 
-    override val childNodes: List<TreeNode>? = null
-
-    override val attributes: Map<String, String>? = null
     override fun toString(): String {
-        return super.toString()
+        return """{"type":"$type","value":"$value"}"""
     }
 }
 

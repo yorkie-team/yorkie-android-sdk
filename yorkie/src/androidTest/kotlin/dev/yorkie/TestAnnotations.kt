0543a79aeb7e1b45bd421f032a031695451a9d7c
jee-hyun-kim <jee_hyun.kim@navercorp.com>
2024-06-24 11:29:54 +0900

    update

--- yorkie/src/androidTest/kotlin/dev/yorkie/TestAnnotations.kt
+++ yorkie/src/androidTest/kotlin/dev/yorkie/TestAnnotations.kt
@@ -2,6 +2,9 @@
 
 package dev.yorkie
 
+/**
+ * Annotation to prevent TooManyRequestsException in JsonTree-related tests
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class TreeTest

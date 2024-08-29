6f98ad6325e7ab11e17317d75732ee17831f8fee
dyl <daeyoung.lee@navercorp.com>
2024-08-29 18:11:36 +0900

    Removed unused function

--- yorkie/src/main/kotlin/dev/yorkie/util/ConnectRpcErrorUtil.kt
+++ yorkie/src/main/kotlin/dev/yorkie/util/ConnectRpcErrorUtil.kt
@@ -21,15 +21,3 @@ fun isRetryable(exception: ConnectException?): Boolean {
 
     return isRetryableErrorCode
 }
-
-
-/**
- * `errorCodeOf` returns the error code of the given connect error.
- */
-fun errorCodeOf(error: ConnectException): String {
-    // NOTE(hackerwins): Currently, we only use the first detail to represent the
-    // error code.
-    val errorDetails = error.unpackedDetails(ConnectErrorDetail::class)
-//    error.withErrorDetails()
-    return "";
-}

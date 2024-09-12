12aa5ef39f6d97a45a0957d94dd7691c13eada87
dyl <daeyoung.lee@navercorp.com>
2024-09-12 19:35:56 +0900

    Updated comment

--- yorkie/src/main/kotlin/dev/yorkie/util/ConnectRpcErrorUtil.kt
+++ yorkie/src/main/kotlin/dev/yorkie/util/ConnectRpcErrorUtil.kt
@@ -5,7 +5,7 @@ import com.connectrpc.ConnectErrorDetail
 import com.connectrpc.ConnectException
 
 /**
- * `isRetryable` will return true if the given error is retryable.
+ * [isRetryable] will return true if the given error is retryable.
  */
 public fun isRetryable(exception: ConnectException?): Boolean {
     val errorCode = exception?.code ?: return false

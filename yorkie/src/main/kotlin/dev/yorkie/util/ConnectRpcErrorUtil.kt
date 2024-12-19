e770e4c7795ba5da6e3cd4756a2eded484501b19
dyl <113019660+daeyounglnc@users.noreply.github.com>
2024-12-20 06:11:54 +0900

    Handle ErrClientNotActivated and ErrClientNotFound (#213)
    * Handle ErrClientNotActivated and ErrClientNotFound
    
    * Added proto file.
    
    * Applied suggestion

--- yorkie/src/main/kotlin/dev/yorkie/util/ConnectRpcErrorUtil.kt
+++ yorkie/src/main/kotlin/dev/yorkie/util/ConnectRpcErrorUtil.kt
@@ -1,16 +1,44 @@
 package dev.yorkie.util
 
 import com.connectrpc.Code
-import com.connectrpc.ConnectErrorDetail
 import com.connectrpc.ConnectException
+import com.google.rpc.ErrorInfo
+import dev.yorkie.util.YorkieException.Code.ErrClientNotActivated
+import dev.yorkie.util.YorkieException.Code.ErrClientNotFound
 
 /**
- * [isRetryable] will return true if the given error is retryable.
+ * [handleConnectException] will return true if the given error is retryable.
+ *
+ * If caller want to handle the error about [ErrClientNotActivated] or [ErrClientNotFound],
+ * then pass the lambda function to [handleError].
  */
-public fun isRetryable(exception: ConnectException?): Boolean {
+internal suspend fun handleConnectException(
+    exception: ConnectException?,
+    handleError: (suspend () -> Unit)? = null,
+): Boolean {
     val errorCode = exception?.code ?: return false
-    return errorCode == Code.CANCELED ||
+
+    if (errorCode == Code.CANCELED ||
         errorCode == Code.UNKNOWN ||
         errorCode == Code.RESOURCE_EXHAUSTED ||
         errorCode == Code.UNAVAILABLE
+    ) {
+        return true
+    }
+
+    val yorkieErrorCode = errorCodeOf(exception)
+    if (yorkieErrorCode == ErrClientNotActivated.codeString ||
+        yorkieErrorCode == ErrClientNotFound.codeString
+    ) {
+        handleError?.invoke()
+    }
+    return false
+}
+
+private fun errorCodeOf(exception: ConnectException): String {
+    val infos = exception.unpackedDetails(ErrorInfo::class)
+    for (info in infos) {
+        info.metadataMap["code"]?.let { return it }
+    }
+    return ""
 }

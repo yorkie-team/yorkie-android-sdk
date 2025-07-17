09afa6f4d097a62945016cf658b9c39dfe480941
ntcong1997 <33303566+ntcong1997@users.noreply.github.com>
2025-07-17 08:57:09 +0700

    Sync android-sdk with js-sdk: Implement updates to v0.6.2 (#224)
    * Handle Subscription limit per Document

--- yorkie.rb
+++ yorkie.rb
@@ -1,8 +1,8 @@
 class Yorkie < Formula
   desc "Document store for collaborative applications"
   homepage "https://yorkie.dev/"
-  url "https://github.com/yorkie-team/yorkie/archive/refs/tags/v0.5.7.tar.gz"
-  sha256 "1f89bb2dad546944b2f987a223b630a129142440ff55180e56e55044cabe0766"
+  url "https://github.com/yorkie-team/yorkie/archive/refs/tags/v0.6.2.tar.gz"
+  sha256 "27c8de8bdd20e8caa5c94bd0694c99ca82bc46355f185cf6be8d482a781b9c96"
   license "Apache-2.0"
   head "https://github.com/yorkie-team/yorkie.git", branch: "main"
 
@@ -12,12 +12,12 @@ class Yorkie < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ce5e9f6b2f5c58242f462f79e88130cfa8f1cac6daed6f136ba2c215b5b1b8b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4ce5e9f6b2f5c58242f462f79e88130cfa8f1cac6daed6f136ba2c215b5b1b8b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "4ce5e9f6b2f5c58242f462f79e88130cfa8f1cac6daed6f136ba2c215b5b1b8b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a1fcf274b4fbd831563604e1f1e6f9e5368ba96f1ebf0f75cbc266dc4283b471"
-    sha256 cellar: :any_skip_relocation, ventura:       "a1fcf274b4fbd831563604e1f1e6f9e5368ba96f1ebf0f75cbc266dc4283b471"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fa519d6bf59d80ddc4b37d4ea294e7726989cd335568ea340eac3c55bd3103c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "75c106672d073c6e92467a06cae7ec31ad51dae7153775fe7b4ea67e8e129147"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75c106672d073c6e92467a06cae7ec31ad51dae7153775fe7b4ea67e8e129147"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "75c106672d073c6e92467a06cae7ec31ad51dae7153775fe7b4ea67e8e129147"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8b08279056ed35759316dfe256be060415b46d8f1d6d2ebf8d4091c5cb800128"
+    sha256 cellar: :any_skip_relocation, ventura:       "8b08279056ed35759316dfe256be060415b46d8f1d6d2ebf8d4091c5cb800128"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "65b1fb221056b4efee88a070a3b384e9a89e7642025e98519dd0eed7e31f4035"
   end
 
   depends_on "go" => :build

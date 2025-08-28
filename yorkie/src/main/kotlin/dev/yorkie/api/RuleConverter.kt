8d2d417a432ca23fdd3d1a93858e34f01e3845b8
Chris <ntcong1997@gmail.com>
2025-08-28 09:35:27 +0700

    Support for object, array and enum in ruleset builder

--- yorkie/src/main/kotlin/dev/yorkie/api/RuleConverter.kt
+++ yorkie/src/main/kotlin/dev/yorkie/api/RuleConverter.kt
@@ -7,33 +7,45 @@ internal typealias PBRule = dev.yorkie.api.v1.Rule
 fun List<PBRule>.fromSchemaRules(): List<Rule> {
     return map {
         when (it.type) {
-            "string", "boolean", "integer", "double", "long", "date", "bytes", "null" -> {
+            in Rule.PrimitiveRule.Type.entries.map { it.value } -> {
                 Rule.PrimitiveRule(
                     path = it.path,
                     type = it.type,
                 )
             }
 
-            "object" -> {
+            Rule.ObjectRule.TYPE -> {
                 Rule.ObjectRule(
                     path = it.path,
-                    type = it.type,
                     properties = emptyList(),
                     optional = emptyList(),
                 )
             }
 
-            "array" -> {
+            Rule.ArrayRule.TYPE -> {
                 Rule.ArrayRule(
+                    path = it.path,
+                    items = emptyList(),
+                )
+            }
+
+            in Rule.YorkieTypeRule.Type.entries.map { it.value } -> {
+                Rule.YorkieTypeRule(
                     path = it.path,
                     type = it.type,
                 )
             }
 
-            "yorkie.Text", "yorkie.Tree", "yorkie.Counter", "yorkie.Object", "yorkie.Array" -> {
-                Rule.YorkieTypeRule(
+            Rule.EnumRule.TYPE -> {
+                Rule.EnumRule(
                     path = it.path,
-                    type = it.type,
+                    values = emptyList(),
+                )
+            }
+
+            Rule.UnionRule.TYPE -> {
+                Rule.UnionRule(
+                    path = it.path,
                 )
             }
 

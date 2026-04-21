a3540096c2e315c5f6c6afd8aba2c899702bb7ea
ntcong1997 <33303566+ntcong1997@users.noreply.github.com>
2026-04-21 15:26:47 +0700

    feat: support launching example apps with a "key" intent extra (#298)

--- examples/todomvc/src/main/java/dev/yorkie/example/todomvc/ui/TodoAppHost.kt
+++ examples/todomvc/src/main/java/dev/yorkie/example/todomvc/ui/TodoAppHost.kt
@@ -5,14 +5,22 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import dev.yorkie.example.feature.enterdocumentkey.navigation.ENTER_DOCUMENT_KEY_ROUTE
 import dev.yorkie.example.feature.enterdocumentkey.navigation.enterDocumentKeyScreen
+import dev.yorkie.example.todomvc.ui.todo.navigation.TODO_BASE_ROUTE
 import dev.yorkie.example.todomvc.ui.todo.navigation.navigateToTodo
 import dev.yorkie.example.todomvc.ui.todo.navigation.todoScreen
+import java.net.URLEncoder
+import kotlin.text.Charsets.UTF_8
 
 @Composable
-fun TodoAppHost(navController: NavHostController) {
+fun TodoAppHost(navController: NavHostController, initialDocumentKey: String? = null) {
+    val startDestination = if (initialDocumentKey != null) {
+        "$TODO_BASE_ROUTE?documentKey=${URLEncoder.encode(initialDocumentKey, UTF_8.name())}"
+    } else {
+        ENTER_DOCUMENT_KEY_ROUTE
+    }
     NavHost(
         navController = navController,
-        startDestination = ENTER_DOCUMENT_KEY_ROUTE,
+        startDestination = startDestination,
     ) {
         enterDocumentKeyScreen(
             onNextClick = {
